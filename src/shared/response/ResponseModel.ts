1cd19284c4c0044e967f01d356133b807a8e64ad
DevKike <alvaronarvaez151@gmail.com>
2024-09-07 21:51:44 -0500

    Chore: update manage response using http status code

--- src/shared/response/ResponseModel.ts
+++ src/shared/response/ResponseModel.ts
@@ -1,13 +1,14 @@
 import { Response } from "express";
 import { IResponse } from "../interfaces/IResponse";
+import { HttpStatusCode } from "../httpStatus/HttpStatusCode";
 
 export class ResponseModel implements IResponse {
   async manageResponse(promise: Promise<any>, res: Response): Promise<void> {
     try {
       const result = await promise;
-      res.status(200).json(result);
+      res.status(HttpStatusCode.SUCCESS).json(result);
     } catch (error) {
-      res.status(500).json({ message: "Internal Server Error" });
+      throw error;
     }
   }
 }
