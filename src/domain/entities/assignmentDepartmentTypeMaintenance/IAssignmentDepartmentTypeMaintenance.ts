4eaf8edff508508e19bd5a6ea49517db35297517
DevKike <alvaronarvaez151@gmail.com>
2024-10-23 18:16:23 -0500

    Fix: update status

--- src/domain/entities/assignmentDepartmentTypeMaintenance/IAssignmentDepartmentTypeMaintenance.ts
+++ src/domain/entities/assignmentDepartmentTypeMaintenance/IAssignmentDepartmentTypeMaintenance.ts
@@ -1,4 +1,4 @@
-import { MaintenanceAssignmentStatus } from "../../enums/assignmentDepartmentTypeMaintenanceStatus/AssignmentDepartmentTypeMaintenanceStatus";
+import { AssignmentDepartmentTypeMaintenanceStatus } from "../../enums/assignmentDepartmentTypeMaintenanceStatus/AssignmentDepartmentTypeMaintenanceStatus";
 import { IDepartment } from "../department/IDepartment";
 import { IMaintenance } from "../maintenance/IMaintenance";
 import { IMaintenanceType } from "../maintenanceType/IMaintenanceType";
@@ -6,7 +6,7 @@ import { IMaintenanceType } from "../maintenanceType/IMaintenanceType";
 export interface IAssignmentDepartmentTypeMaintenance {
   id: number;
   assignmentDate: Date;
-  status: MaintenanceAssignmentStatus;
+  status: AssignmentDepartmentTypeMaintenanceStatus;
   priority: number;
   comments: string;
   department: IDepartment;
